c371b62d569afa2eb6bde88b8938a40f2ae15a8e
iyannsanchezzdc4@gmail.com <iyannsanchezzdc4@gmail.com>
2024-11-25 17:57:24 +0100

    Programa 1 finalizado

---
+++ src/programa_1/AC_KOT_PG1.kt
@@ -0,0 +1,15 @@
+package programa_1
+
+fun main() {
+    for (i in 1..100) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            println("triqui")
+        } else if (i % 3 == 0) {
+            println("tri")
+        } else if (i % 5 == 0) {
+            println("qui")
+        } else {
+            println(i)
+        }
+    }
+}
